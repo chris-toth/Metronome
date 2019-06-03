571ad727f0e3f8b087d07835826c6b6f310f728e
chris.toth <chris-toth@hotmail.com>
2019-06-03 11:01:36 -0600

    added project files

---
+++ Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace SimpleCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(12, 65);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Minimum = 332;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(389, 56);
+            this.trackBar1.TabIndex = 1;
+            this.trackBar1.Value = 1000;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(143, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(115, 32);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "60 BPM";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(405, 111);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleCalc
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //https://msdn.microsoft.com/en-us/library/bb383890(v=vs.90).aspx
+        /*
+        OpenFileDialog dialog = new OpenFileDialog();
+        dialog.Filter = "Audio Files (.wav)|*.wav";
+
+
+        if(dialog.ShowDialog() == DialogResult.OK)
+        {
+        string path = dialog.FileName;
+        playSound(path);
+        }*/
+
+    //Function to play the sound using the System.Media.SoundPlayer class
+    //INPUTS: nothing since the sound is embedded in the resource folder
+    //OUTPUTS: sound plays
+    private void playSound()
+        {
+            System.Media.SoundPlayer player =
+                new System.Media.SoundPlayer(Template.Properties.Resources.clap);
+            
+            player.Play();
+        }
+
+        //Every tick will call the playSound void function to play the sound
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            playSound();            
+        }
+
+        //Controllable track bar to change the BPM of the metronome. also display the BPM as a label on screen
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            timer1.Interval = trackBar1.Value;
+            label3.Text = (60000/trackBar1.Value) + " BPM".ToString();
+        }
+    }
+}
